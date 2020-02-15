1c9514d7e25c3341735d85737b80996f955d5ac9
Balazs-D <balazs.danyadi@gmail.com>
2020-02-15 20:59:19 +0100

    react spring testing

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,70 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useSprings } from 'react';
 import '../assets/css/style.css';
 import Main from './Main';
 import axios from 'axios';
 import ron from '../Parks&Recreation/ron01.jpg';
-import { Transition } from 'react-transition-group';
+import { animated, useTransition } from 'react-spring';
+// import { CSSTransition, TransitionGroup   } from 'react-transition-group';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quote: 'quote...',
-      quotes: []
-    };
-  }
+const App = () => {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState();
 
-  componentDidMount() {
-    this.doFetch();
-  }
+  useEffect(() => {
+    doFetch();
+  }, []);
 
-  doFetch = async () => {
+  const doFetch = async () => {
     const res = await axios(
-      'https://ron-swanson-quotes.herokuapp.com/v2/quotes/'
+      'https://ron-swanson-quotes.herokuapp.com/v2/quotes/55'
     );
 
-    const quotesArray = res.data;
+    const incomingQuotes = res.data;
 
-    this.setState(state => ({ quote: quotesArray }));
+    let randomizedQuotes = await incomingQuotes
+      .map(a => ({
+        sort: Math.random(),
+        value: a
+      }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(a => a.value);
 
-    console.log(this.state.quotes);
+    await setQuotes(randomizedQuotes);
+
+    setQuote(incomingQuotes[2]);
+
+    await console.log(randomizedQuotes);
   };
 
-  render() {
-    return (
-      <div className='container'>
-        <img src={ron} alt='' className='imgRon' />
+  const transitions = useTransition(quotes, quote, {
+    from: { marginTop: '-200%' },
+    enter: { marginTop: '0'},
+    leave: { marginTop: '200%' }
+  });
 
-        <div className='quote-win'>
-          <h1 className='quote'>{this.state.quote}</h1>
-          <button className='button' onClick={this.doFetch}>
-            NEXT
-          </button>
-        </div>
+
+
+  return (
+    <div className='container'>
+      <img src={ron} alt='' className='imgRon' />
+
+      <div className='quote-win'>
+        <animated.h1 
+        className='quote'
+        ></animated.h1>
+
+        {transitions.map(({ item, key, props }) => {
+
+         return <animated.h1 key={key} style={props} className='quote'>{item}</animated.h1>
+
+        })}
+
+        <button className='button' onClick={doFetch}>
+          NEXT
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
